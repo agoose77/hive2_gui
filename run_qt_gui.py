2f558f0f965f30c1002730cee213c468934f9431
Angus Hollands <goosey15@gmail.com>
2017-07-11 09:51:57 +0100

    Add scripts & requirements

---
+++ run_qt_gui.py
@@ -0,0 +1,35 @@
+# Import PySide classes
+import ctypes
+import sys
+
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import Qt
+
+from qdarkstyle import load_stylesheet_pyqt5
+from hive2_gui.qt.main_window import MainWindow
+
+
+if __name__ == "__main__":
+    # Create a Qt application
+    app = QApplication(sys.argv)
+    app.setStyleSheet(load_stylesheet_pyqt5())
+
+    # Fix for windows tray icon
+    app_id = 'hive2.hive2.1.0'
+    try:
+        windll = ctypes.windll
+
+    except AttributeError:
+        pass
+
+    else:
+        windll.shell32.SetCurrentProcessExplicitAppUserModelID(app_id)
+
+    window = MainWindow()
+    window.setWindowState(Qt.WindowMaximized)
+
+    window.show()
+
+    # Enter Qt application main loop
+    app.exec_()
+    sys.exit()
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='hive2_gui',
+      version=0.1,
+      description='Hive Nodal Logic GUI',
+      author='Sjoerd De Vries & Angus Hollands',
+      author_email='goosey15+hive2_gui@gmail.com',
+      url='https://github.com/agoose77/hive2_gui',
+      packages=['hive2_gui'],
+      include_package_data=True,
+      scripts=['run_qt_gui.py'],
+
+      # Project uses reStructuredText, so ensure that the docutils get
+      # installed or upgraded on the target machine
+      install_requires=['PyQt5', 'pygments', 'qdarkstyle', 'hive2'],
+      )
